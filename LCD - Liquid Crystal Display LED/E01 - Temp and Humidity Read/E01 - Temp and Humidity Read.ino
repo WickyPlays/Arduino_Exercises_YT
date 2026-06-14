fba408278b1b2ad39e88de5f6fc3fd6b07b69096
WickyPlays <nguyenthienbao8601@gmail.com>
2026-06-14 12:07:27 +0700

    E01 for LCD

---
+++ LCD - Liquid Crystal Display LED/E01 - Temp and Humidity Read/E01 - Temp and Humidity Read.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// DHT setup
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  
+  dht.begin();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp & Humidity");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+  float temp = dht.readTemperature(); // Celsius
+  float hum  = dht.readHumidity();
+
+  // Check if reading failed
+  if (isnan(temp) || isnan(hum)) {
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Error   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Check wiring   ");
+    delay(2000);
+    return;
+  }
+
+  // Display temperature (in Celsius)
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print((char) 223); // degree symbol
+  lcd.print("C   ");
+
+  // Display humidity
+  lcd.setCursor(0, 1);
+  lcd.print("Hum : ");
+  lcd.print(hum);
+  lcd.print("%   ");
+
+  delay(2000);
+}
